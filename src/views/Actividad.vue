19a622bf8f976e52c6ac76611601bc606ed75635
lizmanchego <karinamanchego0416@gmail.com>
2025-08-13 09:22:02 -0500

    FIX] Validacion RED v1.3

--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -46,7 +46,7 @@ export default {
           mensaje_correcto:
             'Ha captado con claridad los conceptos fundamentales del componente formativo.',
           mensaje_incorrecto:
-            'Respuesta incorrecta, revise nuevamente el contenido del componente formativo.',
+            'Revise nuevamente el contenido del componente formativo.',
         },
         {
           id: 2,
@@ -69,7 +69,7 @@ export default {
           mensaje_correcto:
             'Ha captado con claridad los conceptos fundamentales del componente formativo.',
           mensaje_incorrecto:
-            'Respuesta incorrecta, revise nuevamente el contenido del componente formativo.',
+            'Revise nuevamente el contenido del componente formativo.',
         },
         {
           id: 3,
@@ -92,7 +92,7 @@ export default {
           mensaje_correcto:
             'Ha captado con claridad los conceptos fundamentales del componente formativo.',
           mensaje_incorrecto:
-            'Respuesta incorrecta, revise nuevamente el contenido del componente formativo.',
+            'Revise nuevamente el contenido del componente formativo.',
         },
         {
           id: 4,
@@ -115,7 +115,7 @@ export default {
           mensaje_correcto:
             'Ha captado con claridad los conceptos fundamentales del componente formativo.',
           mensaje_incorrecto:
-            'Respuesta incorrecta, revise nuevamente el conteido del componente formativo.',
+            'Revise nuevamente el conteido del componente formativo.',
         },
         {
           id: 5,
@@ -138,7 +138,7 @@ export default {
           mensaje_correcto:
             'Ha captado con claridad los conceptos fundamentales del componente formativo.',
           mensaje_incorrecto:
-            'Respuesta incorrecta, revise nuevamente el contenido del componente formativo.',
+            'Revise nuevamente el contenido del componente formativo.',
         },
         {
           id: 6,
@@ -161,7 +161,7 @@ export default {
           mensaje_correcto:
             'Ha captado con claridad los conceptos fundamentales del componente formativo.',
           mensaje_incorrecto:
-            'Respuesta incorrecta, revise nuevamente el contenido del componente formativo.',
+            'Revise nuevamente el contenido del componente formativo.',
         },
         {
           id: 7,
@@ -184,7 +184,7 @@ export default {
           mensaje_correcto:
             'Ha captado con claridad los conceptos fundamentales del componente formativo.',
           mensaje_incorrecto:
-            'Respuesta incorrecta, revise nuevamente el contenido del componente formativo.',
+            'Revise nuevamente el contenido del componente formativo.',
         },
         {
           id: 8,
@@ -207,7 +207,7 @@ export default {
           mensaje_correcto:
             'Ha captado con claridad los conceptos fundamentales del componente formativo.',
           mensaje_incorrecto:
-            'Respuesta incorrecta, revise nuevamente el contenido del componente formativo.',
+            'Revise nuevamente el contenido del componente formativo.',
         },
         {
           id: 9,
@@ -230,7 +230,7 @@ export default {
           mensaje_correcto:
             'Ha captado con claridad los conceptos fundamentales del componente formativo.',
           mensaje_incorrecto:
-            'Respuesta incorrecta, revise nuevamente el contenido del componente formativo.',
+            'Revise nuevamente el contenido del componente formativo.',
         },
         {
           id: 10,
@@ -253,7 +253,7 @@ export default {
           mensaje_correcto:
             'Ha captado con claridad los conceptos fundamentales del componente formativo.',
           mensaje_incorrecto:
-            'Respuesta incorrecta, revise nuevamente el contenido del componente formativo.',
+            'Revise nuevamente el contenido del componente formativo.',
         },
       ],
       mensaje_final_aprobado: 'Ha superado la actividad.',

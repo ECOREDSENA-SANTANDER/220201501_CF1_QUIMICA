eaf672dfd1691dde4221d188c02760d0cbf9bd99
lizmanchego <karinamanchego0416@gmail.com>
2025-09-08 10:46:58 -0500

    [ADD] CFA

--- src/config/global.js
+++ src/config/global.js
@@ -177,7 +177,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/dist.pdf',
+        download: 'downloads/220201501_CF1_DU.zip',
       },
       {
         icono: 'fas fa-download',
